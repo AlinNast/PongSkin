3fda6cb1cd3d9b5ba651cc0c241f8167438418c9
AlinNast <alintudor.nast@gmail.com>
2026-02-13 08:58:27 +0100

    First build created

--- W0041E-Pong-main/W0041E/Laboratory1/Lab3_GameFeel_Pong/Scripts/Siren.cs
+++ W0041E-Pong-main/W0041E/Laboratory1/Lab3_GameFeel_Pong/Scripts/Siren.cs
@@ -3,14 +3,22 @@ using System;
 
 public partial class Siren : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    bool isSkinOn = true;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-	}
+        this.Visible = isSkinOn;
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		this.GlobalRotate(Vector3.Up, (float)delta);
+        if (Input.IsActionJustPressed("Switch"))
+        {
+            isSkinOn = (isSkinOn) ? false : true;
+            this.Visible = isSkinOn;
+        }
+        
+        this.GlobalRotate(Vector3.Up, (float)delta);
 	}
 }
